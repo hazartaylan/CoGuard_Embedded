8c0849ae12f3d1b1d5061b59ba85462d4cbd2aaa
hazartaylan <57994196+hazartaylan@users.noreply.github.com>
2022-01-15 00:05:37 +0300

    Update coguard_embedded.ino

--- coguard_embedded.ino
+++ coguard_embedded.ino
@@ -1,20 +1,32 @@
+#include "BluetoothSerial.h"
 #define SENSOR A0 // Set the A0 as SENSOR
 char str_sensor[10];
  
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(SENSOR, INPUT);
+   pinMode(SENSOR, INPUT);
+  SerialBT.begin("CoGuard"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
 }
- 
+
 void loop() {
-
-
-  
-  float sensor = analogRead(SENSOR); 
+    float sensor = analogRead(SENSOR); 
   
   /* 4 is mininum width, 2 is precision; float value is copied onto str_sensor*/
   dtostrf(sensor, 4, 2, str_sensor);
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    Serial.write(str_sensor);
+  }
+ 
   Serial.println(str_sensor);
-  delay(100);
+  delay(20);
 }
